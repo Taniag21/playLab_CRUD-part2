fb458c11a69c129513fdeb5797f1ee07e0b56453
Tania Griffin <TaniaG@Tanias-MacBook.local>
2016-12-02 12:30:54 +0000

    First commit

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -17,6 +17,15 @@ import models.*;
 
 public class HomeController extends Controller {
 
+    //Declare a private formFactory instance
+    private FormFactory formFactory;
+
+    //Inject an instance of FormFactory it into the controller via its constructor
+    @Inject
+    public HomeController(FormFactory f) {
+        this.formFactory = f;
+    }
+
     public Result index(String name) {
         return ok(index.render("Welcome to the Home page", name));
     }
@@ -30,6 +39,34 @@ public class HomeController extends Controller {
         // Get list of all categories in ascending order
         List<Product> productsList = Product.findAll();
         return ok(products.render(productsList));
+    }
+
+    public Result addProduct() {
+        Form<Product> addProductForm = formFactory.form(Product.class);
+
+        return ok(addProduct.render(addProductForm));
+    }
+
+    public Result addProductSubmit() {
+        Form<Product> newProductForm = formFactory.form(Product.class).bindFromRequest();
+
+        if (newProductForm.hasErrors()) {
+            return badRequest(addProduct.render(newProductForm));
+        }
+        Product newProduct = newProductForm.get();
+
+        newProduct.save();
+        flash("success", "Product " + newProduct.getName() + " has been created");
+
+        return redirect(routes.HomeController.products());
+
+    }
+
+    public Result deleteProduct(Long id) {
+        Product.find.ref(id).delete();
+        flash("success", "Product has been deleted");
+
+        return redirect(routes.HomeController.products());
     }
 
 }
